a8051f533cafc95dba41ee79827d2bf036dd6881
Shuyen Lo <sycw723@gmail.com>
2024-06-13 15:14:17 +0800

    Add signin and register UI

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -85,6 +85,33 @@ async function renderMrt() {
     return;
 }
 
+const signinBox = document.querySelector("#signin-box");
+const registerBox = document.querySelector("#register-box");
+const mask = document.querySelector(".fullScreenMask");
+
+function closeSignBox() {
+    const signs = document.querySelectorAll(".sign");
+    for(let sign of signs) {
+        sign.style.display = "none";
+    }
+    mask.style.display = "none";
+    document.body.style.overflow = '';
+}
+
+function openSigninBox() {
+    signinBox.style.display = "block";
+    registerBox.style.display = "none";
+    mask.style.display = "block";
+    document.body.style.overflow = 'hidden';
+}
+
+function openRegisterBox() {
+    signinBox.style.display = "none";
+    registerBox.style.display = "block";
+    mask.style.display = "block";
+    document.body.style.overflow = 'hidden';
+}
+
 window.addEventListener("load", async function() {
     renderMrt();
     renderAttraction(keyword);
