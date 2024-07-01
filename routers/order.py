57ab6766c8119017a5c16617dbf570c8697b5782
Shuyen Lo <sycw723@gmail.com>
2024-07-01 13:00:29 +0800

    Add order backend API

--- routers/order.py
+++ routers/order.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from typing import Annotated
+from routers.user import get_auth_user, UserOutput
+from views.order import Order, PaymentResult, OrderPaid
 
 router = APIRouter()
 
+@router.post("/api/orders")
+def create_new_order(order: Order, user: Annotated[UserOutput, Depends(get_auth_user)]) -> PaymentResult:
+    if not user:
+        return JSONResponse(status_code=403, content={"error": True, "message": "未登入系統，拒絕存取"})
+    
+@router.get("/api/order/{orderNumber}")
+def get_order(orderNumber: str, user: Annotated[UserOutput, Depends(get_auth_user)]) -> OrderPaid:
+    if not user:
+        return JSONResponse(status_code=403, content={"error": True, "message": "未登入系統，拒絕存取"})
+    
---
+++ views/order.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, EmailStr
+from views.attraction import AttractionBase
+
+class Contact(BaseModel):
+    name: str
+    email: EmailStr
+    phone: str
+
+class Trip(BaseModel):
+    attraction: AttractionBase
+    date: str
+    time: str
+
+class OrderBase(BaseModel):
+    price: int
+    trip: Trip
+    contact: Contact
+
+class Order(BaseModel):
+    prime: str
+    order: OrderBase
+
+class PaymentBase(BaseModel):
+    status: int
+    message: str
+
+class PaymentData(BaseModel):
+    number: str
+    payment: PaymentBase
+
+class PaymentResult(BaseModel):
+    data: PaymentData
+
+class OrderPaidData(OrderBase):
+    number: str
+    status: int
+
+class OrderPaid(BaseModel):
+    data: OrderPaidData
